4443d28566be1ac57bc37cc9bfcda261cb77f395
rajsinghaniyag18 <rajsighaniyag18sqlandpowerbi@gmail.com>
2026-01-21 19:38:09 +0530

    Updated twice

--- java.java
+++ java.java
@@ -2,4 +2,11 @@ class Java{
 public static void main(String[] args){
 System.out.println("Hello");
 }
+
+public void m2(){
+    // New method added 
+}
+public void m3(){
+    // New Method added.
+}
 }
